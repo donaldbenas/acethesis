9605c9a57a87d756ef02f0f7ac17ba4c28f23254
DonDon <donaldbenas@gmail.com>
2013-03-05 13:33:20 -0800

    udpate

--- application/views/admin/navbar.php
+++ application/views/admin/navbar.php
@@ -10,38 +10,31 @@
 	  <div class="nav-collapse collapse">
 		<ul class="nav">
 		  <li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Sales <b class="caret"></b></a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Transaction <b class="caret"></b></a>
 			<ul class="dropdown-menu">
-			  <li><a href="<?php echo base_url()."sales/cash" ?>">Cash Rigester</a></li>
-			  <li><a href="<?php echo base_url()."sales/daily" ?>">Daily Sales</a></li>
-			  <li><a href="<?php echo base_url()."sales/quick" ?>">Quick Stock Count</a></li>
-			  <li><a href="<?php echo base_url()."sales/inventory" ?>">Inventory</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Ordinary Customer</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Regular Customer</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Supplier Purchase</a></li>
 			  
 			</ul>
 		  </li>
 		  <li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Account <b class="caret"></b></a>
+			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Reports <b class="caret"></b></a>
 			<ul class="dropdown-menu">
-			  <li><a href="<?php echo base_url()."account/receivables" ?>">Recievables</a></li>
-			  <li><a href="<?php echo base_url()."account/payables" ?>">Payables</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Daily Sale</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Stock Inventory</a></li>
 			</ul>
 		  </li>
 		  <li class="dropdown">
 			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Management <b class="caret"></b></a>
 			<ul class="dropdown-menu">
-			  <li><a href="<?php echo base_url()."manage/stocks" ?>">Stocks</a></li>
-			  <li><a href="<?php echo base_url()."manage/users" ?>">Users</a></li>
-			  <li><a href="<?php echo base_url()."manage/customers" ?>">Customers</a></li>
-			  <li><a href="<?php echo base_url()."manage/suppliers" ?>">Suppliers</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Users</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Customers</a></li>
+			  <li><a href="<?php echo base_url()."" ?>">Suppliers</a></li>
 			</ul>
 		  </li>
-		  <li class="dropdown">
-			<a href="#" class="dropdown-toggle" data-toggle="dropdown">Transaction <b class="caret"></b></a>
-			<ul class="dropdown-menu">
-			  <li><a href="<?php echo base_url()."transact/purchases" ?>" >Purchases</a></li>
-			  <li><a href="<?php echo base_url()."transact/expenses" ?>">Expenses</a></li>
-			  <li><a href="<?php echo base_url()."transact/drawings" ?>">Drawings</a></li>
-			</ul>
+		  <li>
+			<a href="#">Products</a>
 		  </li>
 		  <li class="">
 			<a href="">About</a>
