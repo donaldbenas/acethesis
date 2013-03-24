7b758d015a12e76e19f8e83356edd097f0b291a2
DonDon <donaldbenas@gmail.com>
2013-03-25 03:31:39 +0800

    update

--- sql/rgs.sql
+++ sql/rgs.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 24, 2013 at 07:02 PM
+-- Generation Time: Mar 24, 2013 at 07:30 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.3.13
 
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS `tbl_customers` (
   `fld_email` varchar(50) DEFAULT NULL,
   `fld_dateCreated` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=357 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=367 ;
 
 --
 -- Dumping data for table `tbl_customers`
@@ -46,22 +46,17 @@ CREATE TABLE IF NOT EXISTS `tbl_customers` (
 
 INSERT INTO `tbl_customers` (`id`, `fld_status`, `fld_firstname`, `fld_middlename`, `fld_lastname`, `fld_address`, `fld_mobile`, `fld_telephone`, `fld_email`, `fld_dateCreated`) VALUES
 (110, 'regular', 'Erick', '', 'Santos', '', '', '', '', '0000-00-00'),
-(111, 'regular', 'Cammile', '', 'Prats', '', '', '', '', '0000-00-00');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_invoiceattachments`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_invoiceattachments` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fld_invoiceID` int(11) DEFAULT NULL,
-  `fld_name` varchar(50) DEFAULT NULL,
-  `fld_size` varchar(10) DEFAULT NULL,
-  `fld_dateCreated` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+(111, 'regular', 'Cammile', '', 'Prats', '', '', '', '', '0000-00-00'),
+(357, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(358, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(359, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(360, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(361, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(362, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(363, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(364, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(365, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00'),
+(366, 'ordinary', NULL, NULL, NULL, NULL, NULL, NULL, NULL, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -98,14 +93,7 @@ CREATE TABLE IF NOT EXISTS `tbl_invoicereceipts` (
   `fld_dateCreated` date DEFAULT NULL,
   `fld_dateCanceled` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=416 ;
-
---
--- Dumping data for table `tbl_invoicereceipts`
---
-
-INSERT INTO `tbl_invoicereceipts` (`id`, `fld_invoiceID`, `fld_orNumber`, `fld_status`, `fld_paid`, `fld_price`, `fld_dateCreated`, `fld_dateCanceled`) VALUES
-(415, 487, NULL, 'paid', NULL, NULL, '2013-03-24', NULL);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=432 ;
 
 -- --------------------------------------------------------
 
@@ -121,14 +109,7 @@ CREATE TABLE IF NOT EXISTS `tbl_invoices` (
   `fld_dueDate` date DEFAULT NULL,
   `fld_active` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=488 ;
-
---
--- Dumping data for table `tbl_invoices`
---
-
-INSERT INTO `tbl_invoices` (`id`, `fld_customerID`, `fld_code`, `fld_dateCreated`, `fld_dueDate`, `fld_active`) VALUES
-(487, NULL, '', '2013-03-24', '0000-00-00', 0);
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=504 ;
 
 -- --------------------------------------------------------
 
@@ -180,39 +161,41 @@ CREATE TABLE IF NOT EXISTS `tbl_products` (
 --
 
 INSERT INTO `tbl_products` (`id`, `fld_productCompanyID`, `fld_name`, `fld_description`, `fld_code`, `fld_dateCreated`, `fld_price`, `fld_amount`, `fld_stockUpdated`) VALUES
-(6, 3, 'Youngs Town Corned beef', '155g', 26, '0000-00-00', 26.00, 80, '2013-03-21'),
-(7, 3, '555 Tuna Adobo Flavor', '155g', 3, '0000-00-00', 22.00, 80, '2013-03-21'),
-(8, 3, '555 Tuna Caldereta Flavor', '155g', 4, '0000-00-00', 22.00, -24, '2013-03-21'),
-(9, 3, '555 Tuna Mechada', '155g', 6, '0000-00-00', 22.00, -146, '2013-03-21'),
-(10, 3, 'Youngs Town Sardines Spicy', '155g', 27, '0000-00-00', 14.50, 80, '2013-03-21'),
-(11, 3, 'Youngs Town SardinesTomato Sauce', '155g', 28, '0000-00-00', 14.50, 80, '2013-03-21'),
-(12, 3, 'Master Sardines Tomato ', '155g', 22, '0000-00-00', 14.00, 80, '2013-03-21'),
-(13, 3, 'Sardines Tomato ', '155g', 23, '0000-00-00', 14.50, 80, '2013-03-21'),
-(14, 3, 'Fiesta Mechado', '210g', 18, '0000-00-00', 47.00, 80, '2013-03-21'),
-(15, 3, 'Sardines Tomato Sauce', '210g', 24, '0000-00-00', 36.00, 80, '2013-03-21'),
-(16, 3, '555 Tuna Mackarel in original flavor', '210g', 5, '0000-00-00', 48.00, 80, '2013-03-21'),
-(17, 3, 'Argentina Meat Loaf', '210g', 12, '0000-00-00', 29.00, 80, '2013-03-21'),
-(18, 3, 'Argentina Corned beef', '210g', 11, '0000-00-00', 46.00, 80, '2013-03-21'),
-(19, 3, 'Argentina Meat Loaf', '155g', 13, '0000-00-00', 34.00, 80, '2013-03-21'),
-(20, 3, 'Argentina Beef Loaf', '150g', 10, '0000-00-00', 16.50, 80, '2013-03-21'),
-(21, 3, 'Angel Evaporada', '170g', 8, '0000-00-00', 19.00, -88, '2013-03-21'),
-(22, 3, 'Alpine Evaporated', '410ml', 7, '0000-00-00', 27.00, -96, '2013-03-21'),
-(23, 3, 'Liberty Condensed', '410ml', 20, '0000-00-00', 48.00, 80, '2013-03-21'),
-(24, 3, 'AngelCondensed', '410ml', 9, '0000-00-00', 40.00, -112, '2013-03-21'),
-(25, 7, 'Holidays Meat sauce', '380g', 30, '0000-00-00', 39.00, 80, '2013-03-21'),
-(26, 3, 'Autralian Karne Norte Corned beef', '380g', 14, '0000-00-00', 38.00, 80, '2013-03-21'),
-(27, 3, 'Winner Meat Loaf', '110g', 25, '0000-00-00', 14.00, 80, '2013-03-21'),
-(28, 3, 'El rancho Corned beef', '155g', 17, '0000-00-00', 16.50, 80, '2013-03-21'),
-(29, 3, 'El rancho Corned beef', '155g', 16, '0000-00-00', 21.00, 80, '2013-03-21'),
-(30, 3, 'Jersey Condensed', '155g', 19, '0000-00-00', 26.50, 80, '2013-03-21'),
-(31, 3, 'Ligo Calmares', '390ml', 21, '0000-00-00', 32.00, 80, '2013-03-21'),
-(32, 3, 'El rancho Corned beef', '155g', 15, '0000-00-00', 26.00, 80, '2013-03-21'),
-(33, 7, 'Huggies Pampers', '4m', 31, '0000-00-00', 16.00, 80, '2013-03-21'),
-(34, 7, 'Happy Napkin', '8pcs', 29, '0000-00-00', 17.00, 80, '2013-03-21'),
-(35, 7, 'Those Days Napkin', '8pcs', 33, '0000-00-00', 20.00, 80, '2013-03-21'),
-(36, 7, 'Surf', 'Blue bar', 32, '0000-00-00', 21.50, 80, '2013-03-21'),
-(37, 7, 'Tide', 'White bar', 34, '0000-00-00', 20.00, 80, '2013-03-21'),
-(38, 8, 'Nestle Cofee Mate ', 'Sachet ', 35, '0000-00-00', 84.00, 80, '2013-03-21');
+(2, 1, 'Hotdog', 'Hot in a dog', 1, '0000-00-00', 102.00, 100, '0000-00-00'),
+(5, 2, 'Chesee', 'Chese whiz', 2, '0000-00-00', 45.00, 8, '0000-00-00'),
+(6, 3, 'Youngs Town Corned beef', '155g', 26, '0000-00-00', 26.00, 120, '0000-00-00'),
+(7, 3, '555 Tuna Adobo Flavor', '155grams', 3, '0000-00-00', 22.00, 20, '0000-00-00'),
+(8, 3, '555 Tuna Caldereta Flavor', '155g', 4, '0000-00-00', 22.00, 16, '0000-00-00'),
+(9, 3, '555 Tuna Mechada', '155g', 6, '0000-00-00', 22.00, 35, '0000-00-00'),
+(10, 3, 'Youngs Town Sardines Spicy', '155g', 27, '0000-00-00', 14.50, 110, '0000-00-00'),
+(11, 3, 'Youngs Town SardinesTomato Sauce', '155g', 28, '0000-00-00', 14.50, 26, '0000-00-00'),
+(12, 3, 'Master Sardines Tomato ', '155g', 22, '0000-00-00', 14.00, 50, '0000-00-00'),
+(13, 3, 'Sardines Tomato ', '155g', 23, '0000-00-00', 14.50, 25, '0000-00-00'),
+(14, 3, 'Fiesta Mechado', '210g', 18, '0000-00-00', 47.00, 25, '0000-00-00'),
+(15, 3, 'Sardines Tomato Sauce', '210g', 24, '0000-00-00', 36.00, 20, '0000-00-00'),
+(16, 3, '555 Tuna Mackarel in original flavor', '210g', 5, '0000-00-00', 48.00, 30, '0000-00-00'),
+(17, 3, 'Argentina Meat Loaf', '210g', 12, '0000-00-00', 29.00, 23, '0000-00-00'),
+(18, 3, 'Argentina Corned beef', '210g', 11, '0000-00-00', 46.00, 50, '0000-00-00'),
+(19, 3, 'Argentina Meat Loaf', '155g', 13, '0000-00-00', 34.00, 65, '0000-00-00'),
+(20, 3, 'Argentina Beef Loaf', '150g', 10, '0000-00-00', 16.50, 55, '0000-00-00'),
+(21, 3, 'Angel Evaporada', '170g', 8, '0000-00-00', 19.00, 98, '0000-00-00'),
+(22, 3, 'Alpine Evaporated', '410ml', 7, '0000-00-00', 27.00, 52, '0000-00-00'),
+(23, 3, 'Liberty Condensed', '410ml', 20, '0000-00-00', 48.00, 15, '0000-00-00'),
+(24, 3, 'AngelCondensed', '410ml', 9, '0000-00-00', 40.00, 16, '0000-00-00'),
+(25, 7, 'Holidays Meat sauce', '380grams', 30, '0000-00-00', 39.00, 24, '0000-00-00'),
+(26, 3, 'Autralian Karne Norte Corned beef', '380grams', 14, '0000-00-00', 38.00, 12, '0000-00-00'),
+(27, 3, 'Winner Meat Loaf', '110g', 25, '0000-00-00', 14.00, 25, '0000-00-00'),
+(28, 3, 'El rancho Corned beef', '155g', 17, '0000-00-00', 16.50, 25, '0000-00-00'),
+(29, 3, 'El rancho Corned beef', '155g', 16, '0000-00-00', 21.00, 15, '0000-00-00'),
+(30, 3, 'Jersey Condensed', '155g', 19, '0000-00-00', 26.50, 35, '0000-00-00'),
+(31, 3, 'Ligo Calmares', '390ML', 21, '0000-00-00', 32.00, 5, '0000-00-00'),
+(32, 3, 'El rancho Corned beef', '155g', 15, '0000-00-00', 26.00, 36, '0000-00-00'),
+(33, 7, 'Huggies Pampers', '4M', 31, '0000-00-00', 16.00, 50, '0000-00-00'),
+(34, 7, 'Happy Napkin', '8pcs', 29, '0000-00-00', 17.00, 30, '0000-00-00'),
+(35, 7, 'Those Days Napkin', '8pcs', 33, '0000-00-00', 20.00, 40, '0000-00-00'),
+(36, 7, 'Surf', 'Blue bar', 32, '0000-00-00', 21.50, 35, '0000-00-00'),
+(37, 7, 'Tide', 'White bar', 34, '0000-00-00', 20.00, 60, '0000-00-00'),
+(38, 8, 'Nestle Cofee Mate ', 'Sachet ', 35, '0000-00-00', 84.00, 30, '0000-00-00');
 
 -- --------------------------------------------------------
 
@@ -230,7 +213,7 @@ CREATE TABLE IF NOT EXISTS `tbl_stockproductlogs` (
   `fld_sold` int(11) NOT NULL,
   `fld_dateCreated` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1024 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1059 ;
 
 -- --------------------------------------------------------
 
@@ -259,7 +242,7 @@ CREATE TABLE IF NOT EXISTS `tbl_stockupdatelogs` (
   `fld_dateCreated` date NOT NULL,
   `fld_sold` int(11) NOT NULL,
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
 
 -- --------------------------------------------------------
 
@@ -323,21 +306,6 @@ INSERT INTO `tbl_users` (`fld_userID`, `fld_type`, `fld_username`, `fld_password
 (10, 'owner', 'admin', 'd033e22ae348aeb5660fc2140aec35', 'Admin', 'Admin', 'Admin', 'Admin', '09101234567', '4561234', 'admin@admin.com', '0000-00-00', 1),
 (11, 'employee', 'ace', 'c14f6d18c139fe4631fd0da60a5e54', 'Ace', 'cariño', 'Cortes', 'Baybay City', '09064671783', '9627763', 'aceshang@gmail.com', '0000-00-00', 1),
 (12, 'employee', 'Shemang', 'ca50ee09c3c00f1095d4a6e41d9740', 'Shem', 'Codera', 'Suyom', 'Visca,Baybay Leyte', '09352838741', '3357214', 'shemsuyom@yahoo.com', '0000-00-00', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_usertimesheet`
---
-
-CREATE TABLE IF NOT EXISTS `tbl_usertimesheet` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `fld_userID` int(11) DEFAULT NULL,
-  `fld_status` varchar(50) DEFAULT NULL,
-  `fld_timeIn` time DEFAULT NULL,
-  `fld_timeOut` time DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
